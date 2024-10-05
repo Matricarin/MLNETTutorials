f1bb6027af60d6486316a8fc3f88502d31720d37
Roman Urmancheev <matricarin@gmail.com>
2024-10-05 12:02:23 +0500

    Evaluate method was added

--- SentimentAnalysis/Program.cs
+++ SentimentAnalysis/Program.cs
@@ -33,7 +33,21 @@ internal class Program
         return model;
     }
 
-    private void Evaluate(MLContext mlContext, ITransformer model, IDataView splitTestSet)
+    private static void Evaluate(MLContext mlContext, ITransformer model, IDataView splitTestSet)
+    {
+        Console.WriteLine("=============== Evaluating Model accuracy with Test data===============");
+        IDataView predictions = model.Transform(splitTestSet);
+        CalibratedBinaryClassificationMetrics metrics = mlContext.BinaryClassification.Evaluate(predictions, "Label");
+        Console.WriteLine();
+        Console.WriteLine("Model quality metrics evaluation");
+        Console.WriteLine("--------------------------------");
+        Console.WriteLine($"Accuracy: {metrics.Accuracy:P2}");
+        Console.WriteLine($"Auc: {metrics.AreaUnderRocCurve:P2}");
+        Console.WriteLine($"F1Score: {metrics.F1Score:P2}");
+        Console.WriteLine("=============== End of model evaluation ===============");
+    }
+
+    static void UseModelWithSingleItem(MLContext mlContext, ITransformer model)
     {
     }
 }
