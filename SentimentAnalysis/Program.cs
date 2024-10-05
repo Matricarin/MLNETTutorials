2560943c0a6716e5be85c692062466cd2335b354
Roman Urmancheev <matricarin@gmail.com>
2024-10-05 10:58:30 +0500

    dataset class was added

---
+++ SentimentAnalysis/Program.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.ML;
+using Microsoft.ML.Data;
+using static Microsoft.ML.DataOperationsCatalog;
+
+namespace SentimentAnalysis
+{
+    internal class Program
+    {
+        string _dataPath = Path.Combine(Environment.CurrentDirectory, "Data", "yelp_labelled.txt");
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
