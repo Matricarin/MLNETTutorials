0cd80a8b342fa747a38fb94b06a1c21c732cd9a6
Roman Urmancheev <matricarin@gmail.com>
2024-10-07 16:37:39 +0500

    abstractions were added

--- GitHubIssueClassification/Program.cs
+++ GitHubIssueClassification/Program.cs
@@ -1,9 +1,20 @@
-﻿namespace GitHubIssueClassification
+﻿using Microsoft.ML;
+
+namespace GitHubIssueClassification
 {
     internal class Program
     {
         static void Main(string[] args)
         {
+            string _appPath = Path.GetDirectoryName(Environment.GetCommandLineArgs()[0]) ?? ".";
+            string _trainDataPath = Path.Combine(_appPath, "..", "..", "..", "Data", "issues_train.tsv");
+            string _testDataPath = Path.Combine(_appPath, "..", "..", "..", "Data", "issues_test.tsv");
+            string _modelPath = Path.Combine(_appPath, "..", "..", "..", "Models", "model.zip");
+
+            MLContext _mlContext;
+            PredictionEngine<GitHubIssue, IssuePrediction> _predEngine;
+            ITransformer _trainedModel;
+            IDataView _trainingDataView;
             Console.WriteLine("Hello, ML!");
         }
     }
