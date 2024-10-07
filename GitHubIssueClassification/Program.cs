d6aace2dde38c7aa8a79a79af7551287b43726c5
Roman Urmancheev <matricarin@gmail.com>
2024-10-07 16:47:56 +0500

    data loading was described

--- GitHubIssueClassification/Program.cs
+++ GitHubIssueClassification/Program.cs
@@ -1,21 +1,32 @@
 ﻿using Microsoft.ML;
+using MLTutorialsModels;
 
 namespace GitHubIssueClassification
 {
     internal class Program
     {
+        static string _appPath = Path.GetDirectoryName(Environment.GetCommandLineArgs()[0]) ?? ".";
+        static string _trainDataPath = Path.Combine(_appPath, "..", "..", "..", "Data", "issues_train.tsv");
+        static string _testDataPath = Path.Combine(_appPath, "..", "..", "..", "Data", "issues_test.tsv");
+        static string _modelPath = Path.Combine(_appPath, "..", "..", "..", "Models", "model.zip");
+
+        static MLContext _mlContext;
+        PredictionEngine<GitHubIssue, IssuePrediction> _predEngine;
+        ITransformer _trainedModel;
+        IDataView _trainingDataView;
+
         static void Main(string[] args)
         {
-            string _appPath = Path.GetDirectoryName(Environment.GetCommandLineArgs()[0]) ?? ".";
-            string _trainDataPath = Path.Combine(_appPath, "..", "..", "..", "Data", "issues_train.tsv");
-            string _testDataPath = Path.Combine(_appPath, "..", "..", "..", "Data", "issues_test.tsv");
-            string _modelPath = Path.Combine(_appPath, "..", "..", "..", "Models", "model.zip");
-
-            MLContext _mlContext;
-            PredictionEngine<GitHubIssue, IssuePrediction> _predEngine;
-            ITransformer _trainedModel;
-            IDataView _trainingDataView;
             Console.WriteLine("Hello, ML!");
+            _mlContext = new MLContext(seed: 0);
+            _mlContext.Data.LoadFromTextFile<GitHubIssue>(_trainDataPath, hasHeader: true);
+            var pipeline = ProcessData();
+
+        }
+
+        static IEstimator<ITransformer> ProcessData()
+        {
+
         }
     }
 }
